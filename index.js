349567ec908b51b9ad2a41b89e114cc763328329
Andrew Connally <ajconnally93@gmail.com>
2022-12-11 21:16:16 -0600

    added requirement methods in index.js and an inquirer prompt to create the team

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Index.js
+// Inquirer is main package required, will pass answers through in callback functions
+var inquirer = require('inquirer');
+
+var path = require('path');
+var OUTPUT_DIR = path.resolve(__dirname, 'output');
+var outputPath = path.join(OUTPUT_DIR, 'team.html');
+
+var fs = require('fs');
+var Engineer = require('./lib/Engineer.js');
+var Intern = require('./lib/Intern.js');
+var Manager = require('./lib/Manager.js');
+var getTeam = require('./src/template.js');
+
+teamMembers = [];
+
+
+// start() function will actually be what runs the program. addManager, addEngineer, addIntern, buildHtml will be defined later with the first 3 being inquirer prompts as well
+function start() {
+    function createTeam () {
+
+        inquirer.prompt([{
+          type: "list",
+          message: "Which employee would you like to add?",
+          name: "addPrompt",
+          choices: ["Manager", "Engineer", "Intern", "None"]
+
+        }]).then(function (userInput) {
+          switch(userInput.addPrompt) {
+
+            case "Manager":
+              addManager();
+              break;
+
+            case "Engineer":
+              addEngineer();
+              break;
+
+            case "Intern":
+              addIntern();
+              break;
+    
+            default:
+              buildHtml();
+          }
+        })
+      }
+}
