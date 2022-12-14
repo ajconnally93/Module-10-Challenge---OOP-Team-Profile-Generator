aadfdff8d17e4e68dc664a94f46d1a5e60711e65
Andrew Connally <ajconnally93@gmail.com>
2022-12-14 00:52:00 -0600

    added Manager test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,25 @@
+// follows same general structure as Engineer test, easy to replicate
+
+const Manager = require("../lib/Manager");
+
+test("Tests if we can create an office number", () => {
+    const testOfficeNumber = 2;
+    const employeeInst = new Manager("Andrew", 2, "ajconnally93@gmail.com", testOfficeNumber);
+    expect(employeeInst.officeNumber).toBe(testOfficeNumber);
+});
+
+
+
+// tests officeNumber() and role() functions
+
+test("Testing officeNumber() function", () => {
+    const testOfficeNumber = 2;
+    const employeeInst = new Manager("Andrew", 2, "ajconnally93@gmail.com", testOfficeNumber);
+    expect(employeeInst.officeNumber()).toBe(testOfficeNumber);
+});
+
+test("Testing role() function", () => {
+    const returnVal = "Manager";
+    const employeeInst = new Manager("Andrew", 2, "ajconnally93@gmail.com", 2);
+    expect(employeeInst.role()).toBe(returnVal);
+});
